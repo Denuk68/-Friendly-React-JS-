58b259b0db342d63608d88693fb475a61c7b5172
Dexter <denus.denisovuch@gmail.com>
2020-11-11 23:04:24 +0200

    Added adaptive carousel

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import './About.css'
+import React, { Component } from 'react' ;
+import './About.css';
 
 // Owl-carousel
 import OwlCarousel from 'react-owl-carousel';
@@ -8,98 +8,118 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 
 
+export default class About extends Component {      
+    state= {
+        responsive:{
+            0:{
+                items:1,              
+            },
+            768:{
+                items:3,               
+            },
+            992:{
+                items:3,               
+            },
+            1200:{
+                items:3,  
+                dots: true,                               
+            },
+        },
+    };
 
-export default function About() {  
-    return (
-        <>
-        <section className="hero">
-        <div className="container">
-            <div className="hero__row">
-                <div className="hero__body">
-                    <h1>Friendly App</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id
-                        integer.</p>
-                    <button className="btn__get">Get started</button>
-                </div>
-                <div className="hero__image">
-                    <img src="img/about/hero.png" alt="" />
-                </div>
-            </div>
-        </div>
-        </section>
-
-        <section className="block1">
-        <div className="container">
-            <div className="block1__row">
-                <div className="block1__top">
-                    <h2>Our approach to reach your business goals</h2>
-                </div>
-                <div className="block1__bottom">
-                    <div className="block1__item">
-                        <img src="img/about/item1.png" alt=""/>
-                        <h3>Ideate</h3>
-                        <p>Turn your idea from concept to MVP</p>
+    render(){
+        return (
+            <>
+            <section className="hero">
+            <div className="container">
+                <div className="hero__row">
+                    <div className="hero__body">
+                        <h1>Friendly App</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc odio in et, lectus sit lorem id
+                            integer.</p>
+                        <button className="btn__get">Get started</button>
                     </div>
-                    <div className="block1__item">
-                        <img src="img/about/item2.png" alt=""/>
-                        <h3>Design</h3>
-                        <p>Sketch out the product to align the user needs</p>
-                    </div>
-                    <div className="block1__item">
-                        <img src="img/about/item3.png" alt=""/>
-                        <h3>Develop</h3>
-                        <p>Convert the designs into a live application</p>
-                    </div>
-                    <div className="block1__item">
-                        <img src="img/about/item4.png" alt=""/>
-                        <h3>Deploy</h3>
-                        <p>Launching the application to the market</p>
+                    <div className="hero__image">
+                        <img src="img/about/hero.png" alt="" />
                     </div>
                 </div>
             </div>
-        </div>
-        </section>
-
-        <section className="block2">
-        <div className="container">
-            <div className="block2__row">
-                <div className="block2__top">
-                    <h2>All app screens</h2>
-                </div>
-                <OwlCarousel    
-                    className="owl-theme"
-                    loop = {true}
-                    margin={10}
-                    dots = {true}
-                    items= {3}                                               
-                >
-                <div className="block2__bottom">
-                    <div><img src="img/about/screen.png" alt=""/></div>                  
-                </div>
-                </OwlCarousel>
-            </div>
-        </div>
-        </section>
-
-
-        <section className="contact">
-        <div className="container">
-            <div className="contact__row">
-                <div className="contact__image">
-                    <img src="img/about/Creative.png" alt=""/>
-                </div>
-                <div className="contact__body">
-                    <h2>Contact Form</h2>
-                    <form action="">
-                        <input type="text" placeholder="Name"/>
-                        <input type="text" placeholder="Number"/>
-                        <input type="text" placeholder="Email"/>
-                        <button type="submit">Send</button>
-                    </form>
+            </section>
+    
+            <section className="block1">
+            <div className="container">
+                <div className="block1__row">
+                    <div className="block1__top">
+                        <h2>Our approach to reach your business goals</h2>
+                    </div>
+                    <div className="block1__bottom">
+                        <div className="block1__item">
+                            <img src="img/about/item1.png" alt=""/>
+                            <h3>Ideate</h3>
+                            <p>Turn your idea from concept to MVP</p>
+                        </div>
+                        <div className="block1__item">
+                            <img src="img/about/item2.png" alt=""/>
+                            <h3>Design</h3>
+                            <p>Sketch out the product to align the user needs</p>
+                        </div>
+                        <div className="block1__item">
+                            <img src="img/about/item3.png" alt=""/>
+                            <h3>Develop</h3>
+                            <p>Convert the designs into a live application</p>
+                        </div>
+                        <div className="block1__item">
+                            <img src="img/about/item4.png" alt=""/>
+                            <h3>Deploy</h3>
+                            <p>Launching the application to the market</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-        </section>
-        </>
-    )
-}
+            </section>
+    
+            <section className="block2">
+            <div className="container">
+                <div className="block2__row">
+                    <div className="block2__top">
+                        <h2>All app screens</h2>
+                    </div>
+                    <OwlCarousel    
+                        className="owl-theme" 
+                        loop={true}                              
+                        responsive={this.state.responsive}   
+                                                                                
+                    >
+                    <div className="block2__bottom">
+                        <div><img src="img/about/screen.png" alt=""/></div>                  
+                    </div>
+                    </OwlCarousel>
+                </div>
+            </div>
+            </section>
+    
+    
+            <section className="contact">
+            <div className="container">
+                <div className="contact__row">
+                    <div className="contact__image">
+                        <img src="img/about/Creative.png" alt=""/>
+                    </div>
+                    <div className="contact__body">
+                        <h2>Contact Form</h2>
+                        <form action="">
+                            <input type="text" placeholder="Name"/>
+                            <input type="text" placeholder="Number"/>
+                            <input type="text" placeholder="Email"/>
+                            <button type="submit">Send</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            </section>
+            </>
+        )
+    }
+};
+
+  
